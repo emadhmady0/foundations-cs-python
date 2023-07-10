5c703055b9eaee387634c6a829130e29c8463c54
Emad Hmady <emadhmady0@gmail.com>
2023-07-10 18:17:42 +0300

    findMax function code

--- assignment_02_Emad_Hmady/assignment_02_Emad_Hmady.py
+++ assignment_02_Emad_Hmady/assignment_02_Emad_Hmady.py
@@ -15,6 +15,18 @@ def countDigits(num, counter):
         counter += 1
         return countDigits(num//10, counter)
 
+def findMax(lst):
+    if (len(lst) == 0):
+        return 0
+    elif (len(lst) == 1):
+        return str(lst[0])
+    else:
+        if (lst[0] >= lst[1]):
+            lst.pop(1)
+        else:
+            lst.pop(0)
+        return findMax(lst)
+
 
 def main():
     displayMenu()
@@ -27,5 +39,20 @@ def main():
         case 1:
             count_digits_user_input = int(input("Enter a number: "))
             print(countDigits(count_digits_user_input, 0))
+        case 2:
+            user_list = []
+            number_of_items_in_list_user_input = int(input(
+                "How many numbers do you want to add to your list: "))
+            while (number_of_items_in_list_user_input < 0):
+                print("\nOnly numbers greater than or equal to zero are allowed \n")
+                number_of_items_in_list_user_input = int(input(
+                    "How many numbers do you want to add to your list: "))
+            if (number_of_items_in_list_user_input == 0):
+                print(findMax(user_list))
+            else:
+                for i in range(number_of_items_in_list_user_input):
+                    numbers_user_input = int(input("Enter a number: "))
+                    user_list.append(numbers_user_input)
+                print(findMax(user_list))
 
 main()
