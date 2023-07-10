2fdb88ade4f87929efc9ef5632910468ca2f14d0
Emad Hmady <emadhmady0@gmail.com>
2023-07-10 18:13:44 +0300

    countDigits function code

--- assignment_02_Emad_Hmady/assignment_02_Emad_Hmady.py
+++ assignment_02_Emad_Hmady/assignment_02_Emad_Hmady.py
@@ -7,6 +7,14 @@ def displayMenu():
           "4. Exit\n" +
           "- - - - - - - - - - - - - - -")
 
+def countDigits(num, counter):
+    if ((num//10) == 0):
+        counter += 1
+        return counter
+    else:
+        counter += 1
+        return countDigits(num//10, counter)
+
 
 def main():
     displayMenu()
@@ -15,6 +23,9 @@ def main():
         print("\nOnly numbers between 1 and 4 are allowed \n")
         displayMenu()
         user_input_menu = int(input("Enter a choice: "))
-
+    match user_input_menu:
+        case 1:
+            count_digits_user_input = int(input("Enter a number: "))
+            print(countDigits(count_digits_user_input, 0))
 
 main()
