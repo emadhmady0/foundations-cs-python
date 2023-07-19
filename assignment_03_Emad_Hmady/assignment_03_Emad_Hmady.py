3291ce4715b3764d694b34f7b6f34ac773cec324
Emad Hmady <emadhmady0@gmail.com>
2023-07-19 18:17:43 +0300

    Defined exportJson function and tranformListToDictionary function

--- assignment_03_Emad_Hmady/assignment_03_Emad_Hmady.py
+++ assignment_03_Emad_Hmady/assignment_03_Emad_Hmady.py
@@ -11,3 +11,18 @@ def sumTuples(tpl1,tpl2):
         lst.append(int(tpl1[i])+int(tpl2[i]))
     tpl3 = tuple(lst)
     return tpl3
+
+def exportJson(dict,filename):
+    f = open(filename,"w")
+    json_text = "{\n"
+    for key,value in dict.items():
+        json_text+=  ("\t"+'"'+ key+'"'+":"+'"'+value+'"'+",\n")
+    json_text = json_text[:len(json_text)-2] + "\n}"
+    f.write(json_text)
+    f.close()
+
+def transformListToDictionary(lst):
+    dicty = {}
+    for i in lst:
+        dicty[i[0]] = i[1]
+    return dicty
