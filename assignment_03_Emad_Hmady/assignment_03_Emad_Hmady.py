c5d5fbc9524a37f5dd6e270a8b995d896ff1fbd2
Emad Hmady <emadhmady0@gmail.com>
2023-07-19 18:20:53 +0300

    Defined and called Main function

--- assignment_03_Emad_Hmady/assignment_03_Emad_Hmady.py
+++ assignment_03_Emad_Hmady/assignment_03_Emad_Hmady.py
@@ -44,3 +44,36 @@ def importJson(filename):
         dic[i[0]] = i[1]
         lst_of_dictionaries.append(dic)
     return lst_of_dictionaries
+
+def main():
+    close_program = False
+    while(close_program == False):
+        printMenu()
+        menu_user_input = int(input("Choose an option from the menu above: "))
+        match (menu_user_input):
+            case 1:
+                frst_tuple_strng = input("input first tuple in the following form: x,y,z...: ")
+                tuple1_lst = frst_tuple_strng.split(",")
+                tuple1 = tuple(tuple1_lst)
+                scnd_tuple_strng = input("input second tuple in the following form: x,y,z...:") 
+                tuple2_lst = scnd_tuple_strng.split(",")
+                tuple2 = tuple(tuple2_lst)
+                print("\n" + str(sumTuples(tuple1,tuple2)) + "\n")
+            case 2:
+                number_of_dict_items_user_input = int(input("how many key:value pairs do you want to add to the Json File? "))
+                dict_list = []
+                for i in range(number_of_dict_items_user_input):
+                    dict_user_input = input("Input a dictionary item in the following form (key:value): ")
+                    dict_list.append(dict_user_input.split(":"))
+                dict1 = transformListToDictionary(dict_list)
+                exportJson(dict1,"assignment_03_Emad_Hmady/text.json")
+                print("\n" + "Data added to file successfully" + "\n")
+            case 3:
+                folder = "assignment_03_Emad_Hmady/"
+                file_name_user_input = input("Enter the name of the Json file: ")
+                directory = folder+file_name_user_input
+                print(importJson(directory))
+            case 4:
+                close_program = True
+            
+main()
