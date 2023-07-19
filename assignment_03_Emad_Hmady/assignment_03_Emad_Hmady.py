d24f18a9ab00e074cd415b7a4cf560646f0047ee
Emad Hmady <emadhmady0@gmail.com>
2023-07-19 18:13:26 +0300

    two json files and printMenu function

---
+++ assignment_03_Emad_Hmady/assignment_03_Emad_Hmady.py
@@ -0,0 +1,6 @@
+def printMenu():
+    print("1. Sum Tuples\n" +
+          "2. Export JSON\n" +
+          "3. Import JSON\n" +
+          "4. Exit\n" +
+          "- - - - - - - - - - - - - - -\n")
